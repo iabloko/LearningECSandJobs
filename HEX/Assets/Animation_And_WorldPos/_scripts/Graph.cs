54d7e180be4a07715dacbdb216ed7e23fe693cda
Dxsunty <Dxsunty@gmail.com>
2018-09-30 10:31:17 +0400

    add simple animation

--- HEX/Assets/Animation_And_WorldPos/_scripts/Graph.cs
+++ HEX/Assets/Animation_And_WorldPos/_scripts/Graph.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Graph : MonoBehaviour {
 
 	public Transform pointPrefab;
-
+	[SerializeField, Range (Mathf.PI, 60 * Mathf.PI)] private float _PI = Mathf.PI;
 	[SerializeField, Range (35, 100)] private int resolution = 35;
 	Transform[] points;
 	public MathFunction _MathFunction;
@@ -11,9 +12,9 @@ public class Graph : MonoBehaviour {
 		Sin,
 		Cos
 	}
-	void Awake () {
 
-		float step = 2f / resolution;
+	void Awake () {
+		float step = 4f / resolution;
 		Vector3 scale = Vector3.one * step;
 		Vector3 position;
 		position.y = 0f;
@@ -21,7 +22,7 @@ public class Graph : MonoBehaviour {
 		points = new Transform[resolution];
 		for (int i = 0; i < points.Length; i++) {
 			Transform point = Instantiate (pointPrefab);
-			position.x = (i + 0.65f) * step - 1f;
+			position.x = (i + 0.5f) * step - 1f;
 			point.localPosition = position;
 			point.localScale = scale;
 			point.SetParent (transform, false);
@@ -29,21 +30,22 @@ public class Graph : MonoBehaviour {
 		}
 	}
 
-	void FixedUpdate () {
+	void Update () {
 		if (_MathFunction == MathFunction.Sin) {
 			for (int i = 0; i < points.Length; i++) {
 				Transform point = points[i];
 				Vector3 position = point.localPosition;
-				position.y = Mathf.Sin (Mathf.PI * (position.x + Time.time));
+				position.y = Mathf.Sin (_PI * (position.x + Time.time / _PI));
 				point.localPosition = position;
 			}
 		} else if (_MathFunction == MathFunction.Cos) {
 			for (int i = 0; i < points.Length; i++) {
 				Transform point = points[i];
 				Vector3 position = point.localPosition;
-				position.y = Mathf.Cos (Mathf.PI * (position.x + Time.time));
+				position.y = Mathf.Cos (_PI * (position.x + Time.time / _PI));
 				point.localPosition = position;
 			}
 		}
+		_PI = Mathf.PingPong (Mathf.PI + Time.time, 55 * Mathf.PI);
 	}
 }
