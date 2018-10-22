4c3e84c24c8ab1737654ebecd7c6c8533c43bd29
iabloko <31312961+iabloko@users.noreply.github.com>
2018-10-22 20:43:38 +0300

    leranPresist

---
+++ HEX/Assets/PersistObj/Game.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	public Transform prefab;
+
+	public KeyCode createKey = KeyCode.C;
+	public KeyCode newGameKey = KeyCode.N;
+
+	List<Transform> objects;
+
+	void Awake () {
+		objects = new List<Transform>();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(createKey)) {
+			CreateObject();
+		}
+		else if (Input.GetKey(newGameKey)) {
+			BeginNewGame();
+		}
+	}
+
+	void BeginNewGame () {
+		for (int i = 0; i < objects.Count; i++) {
+			Destroy(objects[i].gameObject);
+		}
+		objects.Clear();
+	}
+
+	void CreateObject () {
+		Transform t = Instantiate(prefab);
+		t.localPosition = Random.insideUnitSphere * 5f;
+		t.localRotation = Random.rotation;
+		t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+		objects.Add(t);
+	}
+}
