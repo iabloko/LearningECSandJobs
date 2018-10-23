80990dd91c3b6ea97a88859b463a04f2de0648e9
Dxsunty <Dxsunty@gmail.com>
2018-10-23 20:01:09 +0400

    save

--- HEX/Assets/PersistObj/Game.cs
+++ HEX/Assets/PersistObj/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
@@ -7,11 +8,16 @@ public class Game : MonoBehaviour {
 
 	public KeyCode createKey = KeyCode.C;
 	public KeyCode newGameKey = KeyCode.N;
+	public KeyCode saveKey = KeyCode.S;
+	public KeyCode loadKey = KeyCode.L;
 
 	List<Transform> objects;
 
+	string savePath;
+
 	void Awake () {
 		objects = new List<Transform>();
+		savePath = Path.Combine(Application.persistentDataPath, "saveFile");
 	}
 
 	void Update () {
@@ -20,6 +26,12 @@ public class Game : MonoBehaviour {
 		}
 		else if (Input.GetKey(newGameKey)) {
 			BeginNewGame();
+		}
+		else if (Input.GetKeyDown(saveKey)) {
+			Save();
+		}
+		else if (Input.GetKeyDown(loadKey)) {
+			Load();
 		}
 	}
 
@@ -36,5 +48,37 @@ public class Game : MonoBehaviour {
 		t.localRotation = Random.rotation;
 		t.localScale = Vector3.one * Random.Range(0.1f, 1f);
 		objects.Add(t);
+	}
+
+	void Save () {
+		using (
+			var writer = new BinaryWriter(File.Open(savePath, FileMode.Create))
+		) {
+			writer.Write(objects.Count);
+			for (int i = 0; i < objects.Count; i++) {
+				Transform t = objects[i];
+				writer.Write(t.localPosition.x);
+				writer.Write(t.localPosition.y);
+				writer.Write(t.localPosition.z);
+			}
+		}
+	}
+
+	void Load () {
+		BeginNewGame();
+		using (
+			var reader = new BinaryReader(File.Open(savePath, FileMode.Open))
+		) {
+			int count = reader.ReadInt32();
+			for (int i = 0; i < count; i++) {
+				Vector3 p;
+				p.x = reader.ReadSingle();
+				p.y = reader.ReadSingle();
+				p.z = reader.ReadSingle();
+				Transform t = Instantiate(prefab);
+				t.localPosition = p;
+				objects.Add(t);
+			}
+		}
 	}
 }
